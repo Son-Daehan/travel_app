e56765b0439fa02f7ddd7234604d2eec569bf7e1
Samuel Son <Samuel.Daehan@gmail.com>
2022-12-22 18:57:49 -0600

    restaurant card styling

--- frontend/src/pages/home/components/profile/HomePageProfile.jsx
+++ frontend/src/pages/home/components/profile/HomePageProfile.jsx
@@ -1,11 +1,22 @@
+import { useSelector } from "react-redux";
 import "./homepageprofile.css";
 
 const HomePageProfile = () => {
+	const { userInfo, profileImg } = useSelector((state) => state.user);
+
 	return (
 		<div className="home-page-profile-container">
 			<div className="home-page-profile-wrapper">
-				<div>Img</div>
-				<div>Name</div>
+				<div className="home-page-profile-inner-wrapper">
+					<div className="profile-img-container">
+						<img className="profile-img" src={profileImg.img_url} />
+					</div>
+					<div className="profile-name-container">
+						<h5>
+							{userInfo.firstName} {userInfo.lastName}
+						</h5>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
--- frontend/src/pages/restaurants/components/card/RestaurantCard.jsx
+++ frontend/src/pages/restaurants/components/card/RestaurantCard.jsx
@@ -19,22 +19,41 @@ const RestaurantCard = ({ restaurant, setSingleRestaurantLocation }) => {
 		>
 			<div className="restaurant-card-wrapper">
 				{/* <p>{restaurant.id}</p> */}
-				<p>{restaurant.name}</p>
-				<div className="restaurant-img-container">
-					<img src={restaurant.image_url} className="restaurant-img" />
+				<div className="restaurant-card-wrapper-left">
+					<div className="restaurant-img-container">
+						<img src={restaurant.image_url} className="restaurant-img" />
+					</div>
+					<div className="restaurant-name-container">
+						<h5>{restaurant.name}</h5>
+					</div>
 				</div>
 				{/* <p>{restaurant.image_url}</p> */}
-				<a href={restaurant.url}>
-					<div>Yelp</div>
-				</a>
-				{/* <p>{restaurant.review_count}</p> */}
-				{/* <p>{restaurant.rating}</p> */}
-				<p>{restaurant.price}</p>
-				<div>
-					<p>{restaurant.display_address}</p>
-					<p>{restaurant.display_phone}</p>
-					<p>{restaurant.distance}</p>
+				<div className="restaurant-card-wrapper-middle">
+					<div className="restaurant-card-status-container">
+						{!restaurant.is_closed ? "Open" : "Closed"}
+					</div>
+					{/* <p>{restaurant.review_count}</p> */}
+					{/* <p>{restaurant.rating}</p> */}
+					<div className="restaurant-card-phone-container">
+						<p>{restaurant.display_phone}</p>
+					</div>
+
+					<div className="restaurant-card-address-container">
+						{restaurant.location.display_address.map((address) => {
+							return <div>{address}</div>;
+						})}
+					</div>
+					{/* <p>{restaurant.location.display_address[1]}</p> */}
+				</div>
+				<div className="restaurant-card-wrapper-right">
+					<a href={restaurant.url}>
+						<div>Yelp</div>
+					</a>
+					<div>
+						<p>Distance: {restaurant.distance}</p>
+					</div>
 					<Link to={`/`}>View reviews!</Link>
+					<p>{restaurant.price}</p>
 				</div>
 			</div>
 		</div>
