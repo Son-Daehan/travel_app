26b78797412145de49745bae5db2da6662ce72dd
Samuel Son <Samuel.Daehan@gmail.com>
2022-12-23 12:26:33 -0600

    updates

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6=2l7bo51oqz=@pw-m)j=x@8@irp*(l_fzn&086^@3q88s%jc@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
